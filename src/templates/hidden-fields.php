bba2a3495c4b077fcf6cbc779ebe0e1bfd42de27
Jeremy Lindblom <jeremeamia@gmail.com>
2018-07-25 13:14:07 -0700

    Can toggle showing hidden fields on Example 4

--- src/templates/hidden-fields.php
+++ src/templates/hidden-fields.php
@@ -1,11 +1,24 @@
 <?php if (isset($hiddenFields)): ?>
+    <button id="#showFieldsBtn" class="btn btn-secondary btn-sm mb-3" type="button" data-toggle="collapse" data-target="#hiddenFields" aria-expanded="false" aria-controls="hiddenFields">
+        <i class="fas fa-eye-slash fa-md"></i> Toggle showing hidden fields
+    </button>
 
-    <?php foreach ($hiddenFields as $name => $value): ?>
-        <input type="hidden" id="s3_<?= $name ?>" name="<?= $name ?>" value="<?= $value ?>">
+    <div id="hiddenFields" class="collapse mb-3">
+    <?php foreach (array_chunk($hiddenFields, 2, true) as $group): ?>
+        <div class="row">
+        <?php foreach ($group as $name => $value): ?>
+            <div class="col-md-6 mb-3">
+                <label for="s3_<?= $name ?>"><?= $name ?></label>
+                <input type="text" class="form-control" id="s3_<?= $name ?>" name="<?= $name ?>" value="<?= $value ?>" readonly>
+            </div>
+        <?php endforeach; ?>
+        </div>
     <?php endforeach; ?>
+    </div>
 
     <script>
         $(function() {
+            // Intercept form submission to customize "key" field.
             $('#uploadForm').submit(function() {
                 $('#s3_key').val('assets/' + $('#fileCategory').val() + '/' + $('#fileTitle').val());
                 return true;
