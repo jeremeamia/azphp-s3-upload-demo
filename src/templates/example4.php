d5b7b193f8d5e47dbb776dd2a87249248369b239
Jeremy Lindblom <jeremeamia@gmail.com>
2018-07-25 13:39:41 -0700

    Added additional explanation in Example 4

--- src/templates/example4.php
+++ src/templates/example4.php
@@ -3,10 +3,19 @@
     <div class="col-md-4 order-md-2">
         <h3 class="mb-3"><span class="text-muted">Details <i class="fas fa-info-circle fa-md"></i></span></h3>
         <p>In <b>Example 4</b>, we do NOT proxy the upload to S3 through our server. Instead we use the
-           <code>PostObjectV4</code> object in the AWS SDK for PHP that helps us to create pre-signed form parameters
-           that all our form to POST directly to S3.</p>
-        <p>Because the request is not proxied through our server, it should end up being faster.</p>
-        <p><b>Note:</b> The API for this is pretty complicated. If you are going to do it, I definitely suggest using the SDK.</p>
+            <code>PostObjectV4</code> object in the AWS SDK for PHP that helps us to create pre-authenticated set of
+            form parameters (including a semi-complicated policy) that allows our form to POST directly to S3. Because
+            the request is not proxied through our server, it should be faster (especially for larger uploads).</p>
+        <p>The <code>PostObjectV4</code> generates extra form fields that must be included in the POST. Click on
+            "Toggle showing hidden fields" to view the generated fields.</p>
+        <p><b><var>key</var>:</b> This field determines the object's key in S3. The default value is
+            <code>${filename}</code>, which means that the name of the uploaded file is used as the key. In our
+            example app, we have a separate field for the <code>fileTitle</code> as well as the
+            <code>fileCategory</code> that affect what we want the object's key to be, so we use jQuery to update the
+            <var>key</var> field's value on the form's "submit" event.</p>
+        <p><b><var>file</var></b>: The field name for the actual upload input must be <var>file</var>. Also, it must be
+            the last field in the request (strange S3 restriction), so we have to make sure we put the hidden fields
+            above it, and that the submit button element does not have a <code>name</code> attribute.</p>
     </div>
 
     <div class="col-md-8 order-md-1">
--- src/templates/hidden-fields.php
+++ src/templates/hidden-fields.php
@@ -1,19 +1,21 @@
 <?php if (isset($hiddenFields)): ?>
+
     <button id="#showFieldsBtn" class="btn btn-secondary btn-sm mb-3" type="button" data-toggle="collapse" data-target="#hiddenFields" aria-expanded="false" aria-controls="hiddenFields">
         <i class="fas fa-eye-slash fa-md"></i> Toggle showing hidden fields
     </button>
 
     <div id="hiddenFields" class="collapse mb-3">
-    <?php foreach (array_chunk($hiddenFields, 2, true) as $group): ?>
-        <div class="row">
-        <?php foreach ($group as $name => $value): ?>
-            <div class="col-md-6 mb-3">
-                <label for="s3_<?= $name ?>"><?= $name ?></label>
-                <input type="text" class="form-control" id="s3_<?= $name ?>" name="<?= $name ?>" value="<?= $value ?>" readonly>
+        <?php foreach (array_chunk($hiddenFields, 2, true) as $group): ?>
+            <div class="row">
+            <?php foreach ($group as $name => $value): ?>
+                <div class="col-md-6 mb-3">
+                    <label for="s3_<?= $name ?>"><var><?= $name ?></var></label>
+                    <input type="text" class="form-control" id="s3_<?= $name ?>" name="<?= $name ?>" value="<?= $value ?>" readonly>
+                </div>
+            <?php endforeach; ?>
             </div>
         <?php endforeach; ?>
-        </div>
-    <?php endforeach; ?>
+        <hr class="mb-3">
     </div>
 
     <script>
